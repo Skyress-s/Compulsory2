52c16f70e316d88fc892fca70ce12b0ececb2d04
Staal22 <staalbr@gmail.com>
2022-09-09 11:58:49 +0200

    randomize and print array

--- Compulsory2/ArrayAndLinkedList.h
+++ Compulsory2/ArrayAndLinkedList.h
@@ -6,5 +6,5 @@ public:
     int staticArray[40]{};
 
     void PopulateArrayRand(int min, int max);
-    
+    void PrintArray();
 };
--- Compulsory2/Compulsory2.cpp
+++ Compulsory2/Compulsory2.cpp
@@ -1,6 +1,9 @@
+#include "ArrayAndLinkedList.h"
 
 int main()
 {
-    
+    ArrayAndLinkedList arrLink;
+    arrLink.PopulateArrayRand(0, 99);
+    arrLink.PrintArray();
     return 0;
 }
